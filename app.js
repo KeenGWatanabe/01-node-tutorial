5633ca4aa8e394e6c35634ce90ea3c0d2ff495bc
KeenGWatanabe <mckeenweb@gmail.com>
2023-08-27 14:27:29 +0800

    .

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+// npm - global cmd, comes w node
+// npm --version
+// local dependency - use it only in this ptlar proj
+// npm i <pckgName>
+
+// global dependency - use it in any proj
+// npm install -g <pckgName>
+
+// pkg.json - manifest file (stores impt info abt proj/pckg)
+// manual approach (create pkg.json in the root, create properties)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
+
 console.log('Welcome to Node Tutorial')
 const _ = require('lodash');
 const items = [1, [2, [3, [4]]]]
